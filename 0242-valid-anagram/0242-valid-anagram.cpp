7a46349f1eda45cf26f80253113f5aa30b59d0ca
Priyanka Rao <priyankarao2005@gmail.com>
2026-09-07 20:08:11 +0530

    Time: 2 ms (49.74%), Space: 10 MB (11.33%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        unordered_map<char,int>mp;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            mp[t[i]]--;
+        }
+        for(auto it:mp)
+        {
+            if(it.second!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
