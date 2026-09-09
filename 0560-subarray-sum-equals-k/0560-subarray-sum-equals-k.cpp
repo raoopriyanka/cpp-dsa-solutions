993e076278a081ea7ebe1861b3e58f8f94a8f55b
Priyanka Rao <priyankarao2005@gmail.com>
2026-09-09 19:08:52 +0530

    Time: 42 ms (59.44%), Space: 45.4 MB (55.3%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<long long, int> mp;
-
-        // Empty prefix
+        unordered_map<int, int> mp;
+        
+        // Prefix sum 0 exists once before the array starts
         mp[0] = 1;
-
-        long long prefixSum = 0;
+        
+        int sum = 0;
         int count = 0;
-
-        for (int num : nums) {
-            prefixSum += num;
-
-            // Check if there is a previous prefix sum
-            // such that current - previous = k
-            if (mp.find(prefixSum - k) != mp.end()) {
-                count += mp[prefixSum - k];
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            // Calculate current prefix sum
+            sum += nums[i];
+            
+            // We need a previous prefix sum = sum - k
+            int required = sum - k;
+            
+            // If it exists, those many subarrays have sum k
+            if (mp.find(required) != mp.end()) {
+                count += mp[required];
             }
-
-            // Store current prefix sum
-            mp[prefixSum]++;
+            
+            // Store/update frequency of current prefix sum
+            mp[sum]++;
         }
-
+        
         return count;
     }
 };
