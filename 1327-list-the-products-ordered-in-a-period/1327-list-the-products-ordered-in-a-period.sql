bfd363640320bf88179df4d9d82748c7b6d49b31
Priyanka Rao <priyankarao2005@gmail.com>
2026-09-17 11:13:53 +0530

    Time: 946 ms (24.84%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
+select product_name, sum(unit) unit
+from Products p 
+join Orders o
+on p.product_id = o.product_id
+where year(order_date) = 2020 and month(order_date) = 2
+group by p.product_name
+having sum(unit) > 99
